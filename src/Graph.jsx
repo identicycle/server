fa88c2f2f100fd0597473f6fbf6379258f83ab06
Lisa Nam <lsyn2016@gmail.com>
2022-06-06 11:27:38 -0400

    createNodes & createConnection draw circles & line

--- src/Graph.jsx
+++ src/Graph.jsx
@@ -10,20 +10,39 @@ export default class Graph extends Component {
   }
 
   componentDidMount() {
-    this.createSVG()
+    this.createNodes()
+    this.createConnections()
   }
 
-  createSVG() {
+  createNodes() {
     const width = 600;
     const height = 300;
     const margin = { top: 30, right: 30, bottom: 30, left: 60 }
 
-    var svg = d3.select("#graph")
+    var circle1 = d3.select("#graph")
     .append("circle")
       .attr("cx", 150)
       .attr("cy", 70)
       .attr("r",  10)
+    
+    var circle2 = d3.select("#graph")
+      .append("circle")
+        .attr("cx", 450)
+        .attr("cy", 70)
+        .attr("r",  10)
 
+  }
+
+  createConnections() {
+    var connection = d3.select("#graph")
+      .append('line')
+      .style("stroke", "black")
+      .style("stroke-width", 5)
+      .attr("x1", 150)
+      .attr("y1", 70)
+      .attr("x2", 450)
+      .attr("y2", 70)
+      .attr("stroke", "black");
   }
 
   render() {
