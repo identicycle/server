39dfab7553ec7270ef0c56009fa4d594f20e03c6
Lisa Nam <lsyn2016@gmail.com>
2022-06-06 14:13:53 -0400

    node added in Graph.jsx

--- src/Graph.jsx
+++ src/Graph.jsx
@@ -4,48 +4,54 @@ import React, { Component }  from 'react';
 //Library
 import * as d3 from "d3";
 
+//Sample Data
+import intersectional_node_data from './sample_datas/intersectional_node_data';
+import intersectional_connection_data from './sample_datas/intersectional_connection_data';
+
 export default class Graph extends Component {
   constructor(props) {
-    super(props)
+    super(props);
+    // const width = 1200;
+    // const height = 800;
+    // const margin = { top: 30, right: 30, bottom: 30, left: 60 }
   }
 
   componentDidMount() {
-    this.createNodes()
-    this.createConnections()
+    this.createNodes();
+    this.createConnections();
   }
 
   createNodes() {
-    const width = 600;
-    const height = 300;
-    const margin = { top: 30, right: 30, bottom: 30, left: 60 }
-
-    var circle1 = d3.select("#graph")
-    .append("circle")
-      .attr("cx", 150)
-      .attr("cy", 70)
-      .attr("r",  10)
+    let svg = d3.select("#graph");
     
-    var circle2 = d3.select("#graph")
-      .append("circle")
-        .attr("cx", 450)
-        .attr("cy", 70)
+    for (let id in intersectional_node_data) {
+      let node = intersectional_node_data[id];
+      console.log(node)
+      svg.append("circle")
+        .attr("class", "intersectional-node")
+        .attr("cx", node.x)
+        .attr("cy", node.y)
         .attr("r",  10)
+        .style("fill", "black");
+    }
 
   }
 
   createConnections() {
-    var connection = d3.select("#graph")
-      .append('line')
-      .style("stroke", "black")
-      .style("stroke-width", 5)
-      .attr("x1", 150)
-      .attr("y1", 70)
-      .attr("x2", 450)
-      .attr("y2", 70)
-      .attr("stroke", "black");
+    // let svg = d3.select("#graph");
+
+    // fot
+    // var connection = d3.select("#graph")
+    //   .append('line')
+    //   .style("stroke", "black")
+    //   .style("stroke-width", 5)
+    //   .attr("x1", 150)
+    //   .attr("y1", 70)
+    //   .attr("x2", 450)
+    //   .attr("y2", 70);
   }
 
   render() {
-    return <svg id="graph" width={600} heigh={300}/>
+    return <svg id="graph" width={1200} height={800}/>
   }
 }
