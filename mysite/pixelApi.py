a0392a5aa10f16f314106db2460eb531b54d9a42
Mateo Armas <mate.armas.medina2004@gmail.com>
2023-07-13 11:17:52 -0400

    Keyword algorithm + Json files for each

--- mysite/pixelApi.py
+++ mysite/pixelApi.py
@@ -7,45 +7,58 @@ client_id = 'FAFurpy5mH24X5xCBJY24ZBu'
 client_secret = 'ieItRG4MrbJ50mGUnN0rDxGtPTHxl9OgDYyWQUm5SWNvCelz'
 
 #Reads all .jpg Files from the testDat Folder
-input_dir = Path.cwd()/'dataTest'
+folder = "trash"
+input_dir = Path.cwd()/f'dataTest/{folder}'
 images = list(input_dir.rglob("*.jpg")) #Everything is stored in glass
-
-namelist = list(input_dir.rglob("*.jpg"))
-
-for each_name in namelist:
-    print(Path(each_name).name)
 
 categories = {
   "ewaste": ["technology", "engineering", "machine part", "manufacturing equipment", "mechanic", "machinery", "computer", "computer monitor", "semiconductor", "computer part", "cpu", "industry", "computer chip", "mother board", "circuit board", "telephone", "computer keyboard", "factory", "equipment", "electrical component", "capacitor", "laptop", "data"],
   "glass": ["glass", "bottle", "wine bottle", "transparent", "drink", "jar"],
   "metal": ["metal", "crumpled", "wrinkled", "crushed", "shiny", "gold", "jewelry", "equipment", "industry", "metal ore"],
-  "organic": ["food", "healthy eating", "organic", "freshness", "begetable", "fruit", "apple", "tomato", "healthy lifestyle", "agriculture", "raw potato", "harvesting", "plant", "lime", "vegetarian food", "juicy", "ripe", "pineaple", "desert", "sweet", "anise", "cucumber", "seafood", "fish", "meal", "healthy", "lemon", "cooking", "appetizer", "meat", "chocolate", "salad"],
+  "organic": ["food", "healthy eating", "organic", "freshness", "vegetable", "fruit", "apple", "tomato", "healthy lifestyle", "agriculture", "raw potato", "harvesting", "plant", "lime", "vegetarian food", "juicy", "ripe", "pineaple", "desert", "sweet", "anise", "cucumber", "seafood", "fish", "meal", "healthy", "lemon", "cooking", "appetizer", "meat", "chocolate", "salad"],
   "paper": ["paper", "crumpled", "book", "education", "newspaper", "text", "editorial", "packet", "document", "envelope", "mail", "communication", "crumple", "blank"],
   "plastic": ["plastic", "bottle", "trasparent", "container", "recycling"],
   "trash": ["crumpled", "wrinkled", "bag", "gargabe", "damaged"]
 }
 
-filteredResponse = []
+#Total responses
+totalResponse = {}
+
+#Total Raw Keywords
+totalRawKeywords = {}
 
 #Loops through data requests
 for image in images:
+    
+    #Obtains all the names
+    fileName = Path(image).name
+    
+    #Filtered keywords, scores, and categories
+    filteredResponse = []
+    
     with open(image,'rb') as image:
         data = {'data': image}
         #Returns response as JSON file
         response = requests.post('https://api.everypixel.com/v1/keywords', files=data, auth=(client_id, client_secret)).json()
+        
+    print("Raw Response: ", response, "\n")
+    
+    totalRawKeywords[fileName] = response["keywords"]
 
-    for keywordJSON in response["keywords"]:
-        keyword, score = keywordJSON.values()
+    # for keywordJSON in response["keywords"]:
+    #     keyword, score = keywordJSON.values()
 
-    for category, keywords in categories.items():
-        if keyword in keywords:
-            filteredResponse.append({
-            "keyword": keyword,
-            "score": score,
-            "category": category
-        })
+    #     for category, keywords in categories.items():
+    #         if keyword in keywords:
+    #             filteredResponse.append({
+    #                 "keyword": keyword,
+    #                 "score": score,
+    #                 "category": category
+    #             })
             
-    print("Current response: ", filteredResponse, "\n")
+    # totalResponse[fileName] = filteredResponse
+            
+    # print("Current response: ", filteredResponse, "\n")
 
-with open('keywords.json', 'w') as key_file:
-    json.dump(filteredResponse, key_file, indent=4)
+    with open(f'keywords_{folder}.json', 'w') as key_file:
+        json.dump(totalRawKeywords, key_file, indent=4)
